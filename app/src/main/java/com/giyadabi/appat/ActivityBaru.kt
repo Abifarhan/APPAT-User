caea608e85ab1f4331e7f9a60b3f3dcf22fe0f41
Abi <abifarhan04@gmail.com>
2020-05-25 22:48:29 +0700

    Penambahan Activity Baru

---
+++ app/src/main/java/com/giyadabi/appat/ActivityBaru.kt
@@ -0,0 +1,12 @@
+package com.giyadabi.appat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityBaru : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baru)
+    }
+}
